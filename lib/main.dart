0e665dee14a4089a4e2a2569356f031d48829369
Kento210 <T121015@ed.sus.ac.jp>
2023-09-10 03:45:29 +0900

    試験的に実装（Webページが正常に表示されないバグあり）

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,83 @@
-// FlutterのMaterialデザインライブラリをインポート
+// Flutterの基本ライブラリをインポート
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
-// HTTPリクエストを行うためのライブラリをインポート
+// HTTPリクエスト用のライブラリ
 import 'package:http/http.dart' as http;
-// テキストを音声に変換するためのライブラリをインポート
+// テキストを音声に変換するライブラリ
 import 'package:flutter_tts/flutter_tts.dart';
-// マークダウン表示のためのライブラリをインポート
-import 'package:flutter_markdown/flutter_markdown.dart';
-// webview_flutterパッケージをインポート
+// WebViewを使用するためのライブラリ
 import 'package:webview_flutter/webview_flutter.dart';
+// Webプラットフォームでiframeを使用するためのライブラリ
+import 'dart:html' as html;
+import 'dart:ui' as ui;
 
-// 非同期関数でURLからコンテンツを取得
+// URLからコンテンツを非同期に取得する関数
 Future<String> fetchContent(String url) async {
-  // HTTP GETリクエストを送信
   final response = await http.get(Uri.parse(url));
-  // ステータスコードが200（成功）なら内容を返す
   if (response.statusCode == 200) {
     return response.body;
   } else {
-    // それ以外の場合は例外を投げる
     throw Exception('Failed to load content');
   }
 }
 
-// FlutterTtsクラスのインスタンスを作成
+// FlutterTtsのインスタンスを作成
 FlutterTts flutterTts = FlutterTts();
 
-// 非同期関数でテキストを音声に変換
+// テキストを音声に変換する関数
 speak(String text) async {
   await flutterTts.speak(text);
 }
 
-// 音声を停止する非同期関数
+// 音声を停止する関数
 stopSpeaking() async {
   await flutterTts.stop();
 }
 
-// アプリケーションのエントリーポイント
+// アプリのエントリーポイント
 void main() {
+  // Webプラットフォームでiframeを使用する場合の設定
+  if (kIsWeb) {
+    final html.IFrameElement iFrameElement = html.IFrameElement()
+      ..width = '640'
+      ..height = '360'
+      ..src = 'https://example.com'
+      ..style.border = 'none';
+
+    // iframeをFlutter Webで使用するための設定
+    ui.platformViewRegistry.registerViewFactory(
+      'example-iframe',
+      (int viewId) => iFrameElement,
+    );
+  }
+
   runApp(MyApp());
 }
 
-// MyAppクラス（ステートレスウィジェット）
+// ステートレスウィジェット MyApp
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // MaterialAppウィジェットを返す
     return MaterialApp(
       home: MyHomePage(),
     );
   }
 }
 
-// MyHomePageクラス（ステートフルウィジェット）
+// ステートフルウィジェット MyHomePage
 class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-// _MyHomePageStateクラス（ステートオブジェクト）
+// MyHomePageのステート
 class _MyHomePageState extends State<MyHomePage> {
-  // URLとコンテンツを保持するための変数
   String url = '';
   String content = '';
   WebViewController? _webViewController;
 
   @override
   Widget build(BuildContext context) {
-    // ScaffoldウィジェットでUIを構築
     return Scaffold(
       appBar: AppBar(
         title: Text('DevListen'),
@@ -76,21 +87,20 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            // URLを入力するためのテキストフィールド
+            // URLを入力するテキストフィールド
             TextField(
               decoration: InputDecoration(labelText: 'Enter URL'),
               onChanged: (value) {
-                // テキストが変更されたらステートを更新
                 setState(() {
                   url = value;
                 });
               },
             ),
-            // ボタンを押すとコンテンツを取得して音声で読み上げる
-            // 横並びにするためにRowウィジェットを使用
+            // 操作ボタンを配置するRow
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
+                // コンテンツを取得して音声で読み上げるボタン
                 Flexible(
                   child: ElevatedButton(
                     onPressed: () async {
@@ -104,6 +114,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ),
                 ),
+                // 音声を停止するボタン
                 Flexible(
                   child: ElevatedButton(
                     onPressed: () {
@@ -115,6 +126,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ),
                 ),
+                // WebViewをリセットするボタン
                 Flexible(
                   child: ElevatedButton(
                     onPressed: () {
@@ -130,17 +142,20 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ],
             ),
-
-            // WebViewを追加
+            // WebViewまたはiframeを表示する領域
             Expanded(
               child: url.isNotEmpty
-                  ? WebView(
-                      initialUrl: url,
-                      javascriptMode: JavascriptMode.unrestricted,
-                      onWebViewCreated: (WebViewController webViewController) {
-                        _webViewController = webViewController;
-                      },
-                    )
+                  ? kIsWeb
+                      ? HtmlElementView(
+                          viewType: 'example-iframe',
+                        )
+                      : WebView(
+                          initialUrl: url,
+                          javascriptMode: JavascriptMode.unrestricted,
+                          onWebViewCreated: (WebViewController webViewController) {
+                            _webViewController = webViewController;
+                          },
+                        )
                   : Container(),
             ),
           ],
@@ -149,8 +164,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
-
-
-
 
