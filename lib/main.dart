bfd63164e48eeff81306e1514e0a8720f0fe9009
Kento210 <T121015@ed.sus.ac.jp>
2023-09-10 00:13:06 +0900

    コメントアウトを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,62 @@
+// FlutterのMaterialデザインライブラリをインポート
 import 'package:flutter/material.dart';
+// HTTPリクエストを行うためのライブラリをインポート
 import 'package:http/http.dart' as http;
+// テキストを音声に変換するためのライブラリをインポート
 import 'package:flutter_tts/flutter_tts.dart';
 
+// 非同期関数でURLからコンテンツを取得
 Future<String> fetchContent(String url) async {
+  // HTTP GETリクエストを送信
   final response = await http.get(Uri.parse(url));
+  // ステータスコードが200（成功）なら内容を返す
   if (response.statusCode == 200) {
     return response.body;
   } else {
+    // それ以外の場合は例外を投げる
     throw Exception('Failed to load content');
   }
 }
 
+// FlutterTtsクラスのインスタンスを作成
 FlutterTts flutterTts = FlutterTts();
 
+// 非同期関数でテキストを音声に変換
 speak(String text) async {
   await flutterTts.speak(text);
 }
 
+// アプリケーションのエントリーポイント
 void main() {
   runApp(MyApp());
 }
 
+// MyAppクラス（ステートレスウィジェット）
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // MaterialAppウィジェットを返す
     return MaterialApp(
       home: MyHomePage(),
     );
   }
 }
 
+// MyHomePageクラス（ステートフルウィジェット）
 class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+// _MyHomePageStateクラス（ステートオブジェクト）
 class _MyHomePageState extends State<MyHomePage> {
+  // URLとコンテンツを保持するための変数
   String url = '';
   String content = '';
 
   @override
   Widget build(BuildContext context) {
+    // ScaffoldウィジェットでUIを構築
     return Scaffold(
       appBar: AppBar(
         title: Text('Text to Speech App'),
@@ -49,14 +65,17 @@ class _MyHomePageState extends State<MyHomePage> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
+            // URLを入力するためのテキストフィールド
             TextField(
               decoration: InputDecoration(labelText: 'Enter URL'),
               onChanged: (value) {
+                // テキストが変更されたらステートを更新
                 setState(() {
                   url = value;
                 });
               },
             ),
+            // ボタンを押すとコンテンツを取得して音声で読み上げる
             ElevatedButton(
               onPressed: () async {
                 content = await fetchContent(url);
@@ -70,5 +89,6 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
 
 
